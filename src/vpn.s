f4d3beb15e9530bd610e08fd9575786e37a1fb47
peternaylor <peter@oldtiogafarm.com>
2025-06-07 14:10:02 -0400

    added documentation folder, addressing and netmasking, and rudimentary error checking

--- src/vpn.s
+++ src/vpn.s
@@ -1,6 +1,11 @@
+; IMPORTANT NOTES
 section		.data
+	errmsg			db "An error occurred!", 0x0A
+	errmsgsz		equ $-errmsg
 	tunfpath		db "/dev/net/tun", 0x00
-	tunlink			db "127.0.0.1"
+	tuniaddr		db 192, 168, 4, 174
+	tunnetmask		db 255, 255, 255, 0
+	tuniaddrsz		equ 4
 	tuntitle		dd "tun8"
 	ifr			times 40 db 0
 	AF_INET			equ 2
@@ -21,6 +26,31 @@ section		.data
 	SIOCSIFADDR     	equ 0x8916
 	SIOCGIFDSTADDR  	equ 0x8917
 	SIOCSIFDSTADDR  	equ 0x8918
+	SIOCGIFBRDADDR  	equ 0x8919
+	SIOCSIFBRDADDR  	equ 0x891A
+	SIOCGIFNETMASK  	equ 0x891B
+	SIOCSIFNETMASK  	equ 0x891C
+	SIOCGIFMETRIC   	equ 0x891D
+	SIOCSIFMETRIC   	equ 0x891E
+	SIOCGIFMEM      	equ 0x891F
+	SIOCSIFMEM      	equ 0x8920
+	SIOCGIFMTU      	equ 0x8921
+	SIOCSIFMTU      	equ 0x8922
+	SIOCSIFNAME     	equ 0x8923
+	SIOCSIFHWADDR   	equ 0x8924
+	SIOCGIFENCAP    	equ 0x8925
+	SIOCSIFENCAP    	equ 0x8926
+	SIOCGIFHWADDR   	equ 0x8927
+	SIOCGIFSLAVE    	equ 0x8929
+	SIOCSIFSLAVE    	equ 0x8930
+	SIOCADDMULTI    	equ 0x8931
+	SIOCDELMULTI    	equ 0x8932
+	SIOCGIFINDEX    	equ 0x8933
+	SIOCSIFPFLAGS   	equ 0x8934
+	SIOCGIFPFLAGS   	equ 0x8935
+	SIOCDIFADDR     	equ 0x8936
+	SIOCSIFHWBROADCAST      equ 0x8937
+	SIOCGIFCOUNT    	equ 0x8938
 	TUNSETNOCSUM		equ 1074025672
 	TUNSETDEBUG		equ 1074025673
 	TUNSETIFF		equ 1074025674
@@ -60,6 +90,19 @@ exit:
 	xor	rdi, rdi
 	mov	al, 60
 	syscall
+reperr:
+	push	rbp
+	mov	rsp, rbp
+	xor	rax, rax
+	xor	rdi, rdi
+	xor	rsi, rsi
+	xor	rdx, rdx
+	inc	al
+	inc	dil
+	mov	esi, errmsg
+	mov	dl, errmsgsz
+	syscall
+	jmp	exit
 zero_ifr:
 	push	rbp
 	mov	rbp, rsp
@@ -67,9 +110,10 @@ zero_ifr:
 	xor	rcx, rcx
 	mov	eax, ifr
 	add	eax, 16
-	mov	ecx, 40
+	mov	ecx, 24
 	zifrl:
-	mov	byte [ifr], 0x00
+	mov	byte [eax], 0x00
+	inc	eax
 	dec	ecx
 	jz	zifrend
 	jmp	zifrl
@@ -92,28 +136,76 @@ tunup:
 	mov	esi, TUNSETIFF
 	mov	edx, ifr
 	syscall
+	cmp	rax, 0
+	jl	error
 	mov	al, 16
 	mov	dil, [sockfd]
-	mov	byte [ifr+16], 0x41
+	mov	byte [ifr+16], 0x01
 	mov	esi, SIOCSIFFLAGS
 	mov	edx, ifr
 	syscall
+	cmp	rax, 0
+	jl	error
 	mov	rsp, rbp
 	pop	rbp
 	ret
 tunaddr:
-	;set tun interface address
+	;set tun interface address and netmask
 	push	rbp
 	mov	rbp, rsp
 	xor	rax, rax
 	xor	rdi, rdi
 	xor	rsi, rsi
+	xor	rcx, rcx
 	xor	rdx, rdx
+	xor	r8, r8
+	;zero out the union chunk of the ifreq
 	call	zero_ifr
+	;set address family word BEFORE inet addr
+	mov	word [ifr+16], AF_INET
+	;prepare registers to feed the inet addr into the offset ifreq chunk
+	mov	eax, tuniaddr
+	mov	ebx, ifr
+	add	ebx, 20
+	mov	ecx, tuniaddrsz
+	tunaddrl:
+	mov	r8b, [eax]
+	mov	byte [ebx], r8b
+	inc 	eax
+	inc	ebx
+	dec	ecx
+	jz	tunaddrcl
+	jmp	tunaddrl
+	tunaddrcl:
+	xor	eax, eax
 	mov	al, 16
 	mov	dil, [sockfd]
 	mov	esi, SIOCSIFADDR
 	mov	edx, ifr
+	syscall
+	cmp	rax, 0
+	jl	error
+	; set if netmask
+	call	zero_ifr
+	mov	eax, tunnetmask
+	mov	ebx, ifr
+	add	ebx, 20
+	mov	ecx, tuniaddrsz
+	mov	word [ifr+16], AF_INET
+	tunmaskl:
+	mov	r8b, [eax]
+	mov	byte [ebx], r8b
+	inc	eax
+	inc	ebx
+	dec	ecx
+	jz	tunmaskcl
+	jmp	tunmaskl
+	tunmaskcl:
+	xor	eax, eax
+	mov	al, 16
+	mov	esi, SIOCSIFNETMASK
+	cmp	rax, 0
+	jl	error
 	syscall
 	mov	rsp, rbp
 	pop	rbp
@@ -124,12 +216,18 @@ _start:
 	mov	edi, tunfpath
 	mov	sil, 02
 	syscall
+	cmp	rax, 0
+	jl	error
 	mov	[tunfd], al
 	mov	al, 41
 	mov	edi, AF_INET
 	mov	sil, SOCK_STREAM
 	syscall
+	cmp	rax, 0
+	jl	error
 	mov	[sockfd], al
 	call 	tunup
 	call	tunaddr
 	jmp	exit
+error:
+	call	reperr
